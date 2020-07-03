17faf6b8a18b508feebae7f2015e128e108735fb
riagupta04 <riacse04@gmail.com>
2020-07-03 12:40:38 +0530

    my project

---
+++ jsscript.js
@@ -0,0 +1,91 @@
+function setABlock() {
+	
+	document.getElementById("map").src="images/a-block.png";
+}
+function setBBlock() {
+	
+	document.getElementById("map").src="images/b-block.png";
+}
+function setCBlock() {
+	
+	document.getElementById("map").src="images/c-block.png";
+}
+function setDBlock() {
+	
+	document.getElementById("map").src="images/d-block.png";
+}
+function setEBlock() {
+	
+	document.getElementById("map").src="images/e-block.png";
+}
+function setFBlock() {
+	
+	document.getElementById("map").src="images/f-block.png";
+}
+function setGBlock() {
+	
+	document.getElementById("map").src="images/g-block.png";
+}
+function setCafet() {
+	
+	document.getElementById("map").src="images/cafet.png";
+}
+function setVolley() {
+	
+	document.getElementById("map").src="images/volleyball-ground.png";
+}
+function setTennis() {
+	
+	document.getElementById("map").src="images/tennis-court.png";
+}
+function setTemple() {
+	
+	document.getElementById("map").src="images/temple.png";
+}
+function setTBI() {
+	
+	document.getElementById("map").src="images/tbi.png";
+}
+function setPlacement() {
+	
+	document.getElementById("map").src="images/placement-cell.png";
+}
+function setPharmacy() {
+	
+	document.getElementById("map").src="images/pharmacy-block.png";
+}
+function setParking() {
+	
+	document.getElementById("map").src="images/parking.png";
+}
+function setMPC() {
+	
+	document.getElementById("map").src="images/mpc.png";
+}
+function setGHostel() {
+	
+	document.getElementById("map").src="images/girls-hostel.png";
+}
+function setBHostel() {
+	
+	document.getElementById("map").src="images/boys-hostel.png";
+}
+function setFGround() {
+	
+	document.getElementById("map").src="images/football-ground.png";
+}
+function setFaculty() {
+	
+	document.getElementById("map").src="images/faculty-residence.png";
+}
+function setCGround() {
+	
+	document.getElementById("map").src="images/cricket-ground.png";
+}
+function setAudi() {
+	
+	document.getElementById("map").src="images/auditorium.png";
+}
+function setOGImage() {
+	document.getElementById("map").src="images/kiet hq.png";
+}
